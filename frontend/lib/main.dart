01a2259c257ddf21127541bc811df674349832cf
Rama Alshehri <156553209+csrama@users.noreply.github.com>
2026-07-03 21:27:22 +0300

    Update main.dart

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
+import 'views/splash/splash_screen.dart';
 
+void main() {
+  runApp(const DawaiApp());
+}
+
+class DawaiApp extends StatelessWidget {
+  const DawaiApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const SplashScreen(),
+    );
+  }
+}
