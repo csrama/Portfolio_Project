cbf5f92f753f4500f7696c13fd19daca0c71abc1
Raghad717 <r4gh4dalmalki@gmail.com>
2026-07-13 01:32:27 +0300

    Delete frontend/lib/ config/api_config.dart

--- frontend/lib/ config/api_config.dart
+++
@@ -1,5 +0,0 @@
-class ApiConfig {
-  static const String baseUrl = 'https://your-api.onrender.com'; 
-  static const int connectTimeout = 30000; // 30 sec
-  static const int receiveTimeout = 30000;
-}
