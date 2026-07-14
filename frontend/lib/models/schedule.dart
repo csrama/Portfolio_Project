0c58dd062c82bc1c88faa756f4bf70c14b1cdde1
Raghad717 <r4gh4dalmalki@gmail.com>
2026-07-14 21:55:06 +0300

    Update schedule.dart

--- frontend/lib/models/schedule.dart
+++ frontend/lib/models/schedule.dart
@@ -1,17 +1,1 @@
-class MedicationSummary {
-  final int id;
-  final String name;
-  final String dosage;
-  final String form;
-  final String instructions;
-  final bool isActive;
 
-  MedicationSummary({
-    required this.id,
-    required this.name,
-    required this.dosage,
-    required this.form,
-    required this.instructions,
-    this.isActive = true,
-  });
-}
