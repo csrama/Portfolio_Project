3c2b136292c0b05f8c9b12f352e16f53dee3305f
csrama <ramadafer1@gmail.com>
2026-07-22 05:25:03 +0300

    api config

--- frontend/lib/config/api_config.dart
+++ frontend/lib/config/api_config.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/foundation.dart';
 
+class ApiConfig {
+  static const String _prodUrl = 'https://portfolioproject-production-2b3b.up.railway.app';
+  static const String _localUrl = 'http://10.0.2.2:3000';
 
-class ApiConfig 
-{
-  static String get baseUrl {
-    if (kIsWeb) {
-      return 'https://portfolioproject-production-2b3b.up.railway.app';
-    }
 
-    // أثناء التطوير على محاكي Android
-    return 'http://10.0.2.2:3000';
+  static String get baseUrl 
+  {
+
+    return _prodUrl;
+    
+    
   }
 }
